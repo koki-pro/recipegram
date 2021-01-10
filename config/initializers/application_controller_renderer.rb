6e89c6a0005b36159f60004b1e364e0d2a7be7bc
koki-pro <koki.banana03@gmail.com>
2021-01-10 16:01:33 +0900

    editiコントローラーの定義

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'bec212e569f1dab06ad4ba78ba059bf4ecfe710806518827d7582b75c06d8c9a1820d16c0dcfdd932fb163d081693925a3d36975a6d9f65ddd859b0802e7cb98'
